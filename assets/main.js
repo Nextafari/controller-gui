d8796a3f74f86a5e560a3a0e0ed137879d55daf9
Nextafari <chinedue16@gmail.com>
2021-10-11 14:55:26 +0100

    Rendering the tables dynamically

---
+++ assets/main.js
@@ -0,0 +1,24 @@
+async function InsertTables(url, tableKeypad) {
+    const myTable = tableKeypad.querySelector(".table-btn");
+    const response = await fetch(url);
+    const data = await response.json();
+    // Looping through data from the endpoint
+    for (tableNumber of data) {
+        console.log(tableNumber.table_number)
+        // creating a new button element
+        const tableButton = document.createElement("button");
+        tableButton.className = "table-btn btn btn-md";
+        // Creating a text for the button
+        const btnNode = document.createTextNode("Table " + tableNumber.table_number);
+        // Appending the text to the button
+        tableButton.appendChild(btnNode);
+        // Appending the button to the DOM
+        const element = document.querySelector(".table-keypad");
+        element.appendChild(tableButton);
+        
+    }
+
+    console.log(data);
+}
+
+InsertTables("http://127.0.0.1:8000/restautant_api/all_tables/", document.querySelector(".table-keypad"));
