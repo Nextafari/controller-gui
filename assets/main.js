c689f9a8094282fbc4ce1ec31b2da424b8917cb5
Nextafari <chinedue16@gmail.com>
2021-10-13 14:31:34 +0100

    Added some DOM manipulations via JS

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,31 @@
-async function InsertTables(url, tableKeypad) {
+// Attribute Helper Func
+// function setAttributes(elem, attrs) {
+//     for (let name in attrs) {
+//         elem.setAttribute(name, attrs[name]);
+//     }
+// }
+
+function selectedTables() {
+    let button = document.querySelector(".table-keypad");
+    let btnValue = document.querySelector(".table-btn").attributes.value;
+    let valueContainer = [];
+    button.addEventListener("click", function() {
+        valueContainer.push(btnValue);
+
+    console.log(valueContainer);
+    });
+
+    console.log(btnValue);
+}
+
+
+async function InsertTables(url) {
     // const myTable = tableKeypad.querySelector(".table-btn");
     const response = await fetch(url);
     const data = await response.json();
     // Looping through data from the endpoint
     for (tableNumber of data) {
-        console.log(tableNumber.table_number)
+        console.log(tableNumber.table_number);
         // creating a new button element
         const tableButton = document.createElement("button");
         // Using already styled css classes to style buttons
@@ -13,13 +34,16 @@ async function InsertTables(url, tableKeypad) {
         const btnNode = document.createTextNode("テーブル " + tableNumber.table_number);
         // Appending the text to the button
         tableButton.appendChild(btnNode);
-        // Appending the button to the DOM
+        // Appending the button to the DOM and also assigning values to them
         const element = document.querySelector(".table-keypad");
-        element.appendChild(tableButton);
+        element.appendChild(tableButton).setAttribute('value', tableNumber.table_number);
         
     }
 
     console.log(data);
 }
 
-InsertTables("http://127.0.0.1:8000/restautant_api/all_tables/", document.querySelector(".table-keypad"));
+InsertTables("http://127.0.0.1:8000/restautant_api/all_tables/");
+
+
+
