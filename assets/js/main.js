920f8da01bd1b07c7f0acca5cb536d68f7368eb3
Nextafari <chinedue16@gmail.com>
2021-10-16 18:20:41 +0100

    removed irrelevant stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,18 +79,25 @@ function sendTableNextPage() {
 
 async function InsertTables(url) {
     const response = await fetch(url);
+
     const data = await response.json();
+
     // Looping through data from the endpoint
     for (tableNumber of data) {
         console.log(tableNumber.table_number);
+
         // creating a new button element
         const tableButton = document.createElement("button");
+
         // Using already styled css classes to style buttons
         tableButton.classList = "table-btn btn btn-md";
+
         // Creating a text for the button
         const btnNode = document.createTextNode("テーブル " + tableNumber.table_number);
+
         // Appending the text to the button
         tableButton.appendChild(btnNode);
+        
         // Appending the button to the DOM and also assigning values to them
         const element = document.querySelector(".table-keypad");
         element.appendChild(tableButton).setAttribute('value', tableNumber.table_number);
