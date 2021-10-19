9d69a2b9cc70b980f65faceae2aa1743f7eb0dbc
Nextafari <chinedue16@gmail.com>
2021-10-19 22:03:43 +0100

    Added features to send messages to shut down and start up the robot

---
+++ assets/js/powerJS.js
@@ -0,0 +1,34 @@
+// Turns off the Robot on btn click 
+function turnOff() {
+    let proceedToShutDown = document.getElementById("proceed-btn").value;
+
+    fetch(
+        `http://127.0.0.1:8000/ros_api/shutdown`, {
+            method: "POST",
+            body: JSON.stringify(proceedToShutDown),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+    ).then(response=> {
+        return response.json();
+    })
+}
+
+
+// Turns the robot on by sending a message to it via the backend
+function startRobot() {
+    let turnOnBtn = document.querySelector(".start-btn").value;
+    console.log("This is the turn on butn", turnOnBtn);
+    fetch(
+        `http://127.0.0.1:8000/ros_api/turn_on`, {
+            method: "POST",
+            body: JSON.stringify(proceedToShutDown),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+    ).then(response=> {
+        return response.json();
+    })
+}
