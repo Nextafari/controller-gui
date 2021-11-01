1dd55aa868a25a5d0ea9cbde26c66036ee719655
Nextafari <chinedue16@gmail.com>
2021-11-01 22:57:57 +0100

    Added features for modal and redirect back to home page after confirming tables

--- assets/js/main.js
+++ assets/js/main.js
@@ -175,4 +175,8 @@ function confirmTable() {
     // Deletes the cookie saved in the browser by expiring it
     document.cookie = "selected Tables=;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
+    // using an anon func to redirect users back to index.html 
+    setTimeout(()=> {window.location.replace("index.html");}, 3500)
+    
+
 }
