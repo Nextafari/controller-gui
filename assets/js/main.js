8e83afadcda5d2fdf4df4796f2d4cdccf258ef70
Nextafari <chinedue16@gmail.com>
2021-11-02 11:45:47 +0100

    dynamically generating content for modal to display different texts when button is clicked

--- assets/js/main.js
+++ assets/js/main.js
@@ -176,5 +176,5 @@ function confirmTable() {
     document.cookie = "selected Tables=;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
     // using an anon func to redirect users back to index.html after 3500 mili secs
-    setTimeout(()=> {window.location.replace("index.html");}, 3500)
+    setTimeout(()=> {window.location.replace("index.html");}, 3500);
 }
